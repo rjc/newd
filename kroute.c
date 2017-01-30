cc0c05c6348f8b385b937ed54526b0d83d4f0979
Kenneth R Westerback <kwesterbacK@gmail.com>
2017-01-29 21:54:59 -0500

    More v6 niftyness.

--- kroute.c
+++ kroute.c
@@ -49,8 +49,9 @@ struct {
 } kr_state;
 
 void	kr_dispatch_msg(int, short, void *);
-void	get_rtaddrs(int, struct sockaddr *, struct sockaddr **);
-void	forward_proposal(struct rt_msghdr *, struct sockaddr **);
+int	get_rtaddrs(int, struct sockaddr *, struct sockaddr **);
+void	forward_v4proposal(struct rt_msghdr *, struct sockaddr **);
+void	forward_v6proposal(struct rt_msghdr *, struct sockaddr **);
 
 int
 kr_init(void)
@@ -109,6 +110,7 @@ kr_dispatch_msg(int fd, short event, void *bula)
 	char			*next;
 	ssize_t			 n;
 	size_t			 len, offset;
+	int			 v6;
 
 	if ((n = read(kr_state.fd, &buf, sizeof(buf))) == -1) {
 		if (errno == EAGAIN || errno == EINTR)
@@ -135,13 +137,16 @@ kr_dispatch_msg(int fd, short event, void *bula)
 			continue;
 
 		sa = (struct sockaddr *)(next + rtm->rtm_hdrlen);
-		get_rtaddrs(rtm->rtm_addrs, sa, rti_info);
+		v6 = get_rtaddrs(rtm->rtm_addrs, sa, rti_info);
 
 		switch (rtm->rtm_type) {
 		case RTM_PROPOSAL:
-			log_warnx("I see a RTM_PROPOSAL from %d!",
-			    rtm->rtm_priority);
-			forward_proposal(rtm, rti_info);
+			log_warnx("I see a %s RTM_PROPOSAL from %d!",
+			    v6 ? "IPv6" : "IPv4", rtm->rtm_priority);
+			if (v6)
+				forward_v6proposal(rtm, rti_info);
+			else
+				forward_v4proposal(rtm, rti_info);
 			break;
 		default:
 			/* ignore for now */
@@ -155,23 +160,26 @@ kr_dispatch_msg(int fd, short event, void *bula)
 #define	ROUNDUP(a)	\
     (((a) & (sizeof(long) - 1)) ? (1 + ((a) | (sizeof(long) - 1))) : (a))
 
-void
+int
 get_rtaddrs(int addrs, struct sockaddr *sa, struct sockaddr **rti_info)
 {
-	int	i;
+	int	i, v6 = 0;
 
 	for (i = 0; i < RTAX_MAX; i++) {
 		if (addrs & (1 << i)) {
 			rti_info[i] = sa;
+			if (sa->sa_family == AF_INET6)
+				v6 = 1;
 			sa = (struct sockaddr *)((char *)(sa) +
 			    ROUNDUP(sa->sa_len));
 		} else
 			rti_info[i] = NULL;
 	}
+	return (v6);
 }
 
 void
-forward_proposal(struct rt_msghdr *rtm, struct sockaddr **rti_info)
+forward_v4proposal(struct rt_msghdr *rtm, struct sockaddr **rti_info)
 {
 	struct imsg_v4proposal	 proposal;
 	struct sockaddr_in	*sa_in;
@@ -240,5 +248,78 @@ forward_proposal(struct rt_msghdr *rtm, struct sockaddr **rti_info)
 	}
 
 	main_imsg_compose_engine(IMSG_SEND_V4PROPOSAL, 0, &proposal,
+	    sizeof(proposal));
+}
+
+void
+forward_v6proposal(struct rt_msghdr *rtm, struct sockaddr **rti_info)
+{
+	struct imsg_v4proposal	 proposal;
+	struct sockaddr_in6	*sa_in6;
+
+	memset(&proposal, 0, sizeof(proposal));
+
+	proposal.addrs = rtm->rtm_addrs;
+	proposal.inits = rtm->rtm_inits;
+	proposal.flags = rtm->rtm_flags;
+	proposal.xid = rtm->rtm_seq;
+	proposal.index = rtm->rtm_index;
+	proposal.source = rtm->rtm_priority;
+
+	if (proposal.inits & RTV_MTU) {
+		proposal.mtu = rtm->rtm_rmx.rmx_mtu;
+	}
+
+	if (rti_info[RTAX_STATIC] != NULL) {
+		struct sockaddr_rtstatic *rtstatic;
+		rtstatic = (struct sockaddr_rtstatic *)rti_info[RTAX_STATIC];
+		memcpy(&proposal.rtstatic, rtstatic->sr_static,
+		    sizeof(proposal.rtstatic));
+	}
+	if (rti_info[RTAX_SEARCH] != NULL) {
+		struct sockaddr_rtsearch *rtsearch;
+		rtsearch = (struct sockaddr_rtsearch *)rti_info[RTAX_SEARCH];
+		if (rtsearch->sr_family == AF_INET6)
+			proposal.rtsearch_encoded = 1;
+		memcpy(&proposal.rtsearch, rtsearch->sr_search,
+		    sizeof(proposal.rtsearch));
+	}
+	if (rti_info[RTAX_GATEWAY] != NULL) {
+		sa_in6 = (struct sockaddr_in6 *)rti_info[RTAX_GATEWAY];
+		memcpy(&proposal.gateway, &sa_in6->sin6_addr,
+		    sizeof(proposal.gateway));
+	}
+	if (rti_info[RTAX_IFA] != NULL) {
+		sa_in6 = (struct sockaddr_in6 *)rti_info[RTAX_IFA];
+		memcpy(&proposal.ifa, &sa_in6->sin6_addr,
+		    sizeof(proposal.ifa));
+	}
+	if (rti_info[RTAX_NETMASK] != NULL) {
+		sa_in6 = (struct sockaddr_in6 *)rti_info[RTAX_NETMASK];
+		memcpy(&proposal.mask, &sa_in6->sin6_addr,
+		    sizeof(proposal.mask));
+	}
+	if (rti_info[RTAX_DNS1] != NULL) {
+		sa_in6 = (struct sockaddr_in6 *)rti_info[RTAX_DNS1];
+		memcpy(&proposal.dns1, &sa_in6->sin6_addr,
+		    sizeof(proposal.dns1));
+	}
+	if (rti_info[RTAX_DNS2] != NULL) {
+		sa_in6 = (struct sockaddr_in6 *)rti_info[RTAX_DNS2];
+		memcpy(&proposal.dns2, &sa_in6->sin6_addr,
+		    sizeof(proposal.dns2));
+	}
+	if (rti_info[RTAX_DNS3] != NULL) {
+		sa_in6 = (struct sockaddr_in6 *)rti_info[RTAX_DNS3];
+		memcpy(&proposal.dns3, &sa_in6->sin6_addr,
+		    sizeof(proposal.dns3));
+	}
+	if (rti_info[RTAX_DNS4] != NULL) {
+		sa_in6 = (struct sockaddr_in6 *)rti_info[RTAX_DNS4];
+		memcpy(&proposal.dns4, &sa_in6->sin6_addr,
+		    sizeof(proposal.dns4));
+	}
+
+	main_imsg_compose_engine(IMSG_SEND_V6PROPOSAL, 0, &proposal,
 	    sizeof(proposal));
 }
